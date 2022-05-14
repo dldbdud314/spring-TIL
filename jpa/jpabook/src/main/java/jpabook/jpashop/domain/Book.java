79b4ab9a6acf3079b3ae65ef0ed82c34e2adb143
dldbdud314 <dldbdud314@naver.com>
2022-05-14 11:41:25 +0900

    add 상속관계매핑, baseEntity

---
+++ jpa/jpabook/src/main/java/jpabook/jpashop/domain/Book.java
@@ -0,0 +1,34 @@
+package jpabook.jpashop.domain;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Book extends Item{
+    private String book;
+    private String author;
+    private String isbn;
+
+    public String getBook() {
+        return book;
+    }
+
+    public void setBook(String book) {
+        this.book = book;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+}
