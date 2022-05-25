5a285e0b82c41730e2e9ea2c9b009c4cf91c9836
dldbdud314 <dldbdud314@naver.com>
2022-05-25 10:46:34 +0900

    상품 등록, 수정 추가, PRG 적용

--- spring-mvc/item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ spring-mvc/item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -3,10 +3,10 @@ package hello.itemservice.web.basic;
 import hello.itemservice.domain.item.Item;
 import hello.itemservice.domain.item.ItemRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.annotation.PostConstruct;
 import java.util.List;
@@ -36,9 +36,57 @@ public class BasicItemController {
         return "basic/addForm";
     }
 
+    //@PostMapping("/add")
+    public String addItemV1(@RequestParam String itemName,
+                       @RequestParam Integer price,
+                       @RequestParam Integer quantity,
+                       Model model){
+        Item item = new Item(itemName, price, quantity);
+        itemRepository.save(item);
+
+        model.addAttribute("item", item);
+
+        return "basic/item";
+    }
+
+    //@PostMapping("/add")
+    public String addItemV2(@ModelAttribute("item") Item item){
+        itemRepository.save(item);
+        //model.addAttribute("item", item); //자동 추가, 생략 가능
+
+        return "basic/item";
+    }
+
+    //@PostMapping("/add") //PRG 적용->리다이렉션
+    public String addItemRedirectV1(@ModelAttribute Item item){
+        itemRepository.save(item);
+
+        return "redirect:/basic/items/"+item.getId();
+    }
+
     @PostMapping("/add")
-    public String save(){
-        return "xxx";
+    //RedirectAttributes로 리다이렉트 추가 정보 넘겨
+    public String addItemRedirectV2(@ModelAttribute Item item,
+                                    RedirectAttributes redirectAttributes){
+        Item savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        redirectAttributes.addAttribute("status", true);
+
+        return "redirect:/basic/items/{itemId}"; //->/basic/items/3?status=true
+    }
+
+    @GetMapping("/{itemId}/edit")
+    public String editForm(@PathVariable Long itemId, Model model){
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item", item);
+
+        return "basic/editForm";
+    }
+
+    @PostMapping("/{itemId}/edit")
+    public String edit(@PathVariable Long itemId, @ModelAttribute Item item){
+        itemRepository.update(itemId, item);
+        return "redirect:/basic/items/{itemId}";
     }
 
     /**
