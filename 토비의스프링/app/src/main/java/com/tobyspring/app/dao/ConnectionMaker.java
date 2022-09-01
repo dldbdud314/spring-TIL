3b2a8d8d86e5ce3731e2825b964a10f85898337d
Yooyoung Lee <57944099+dldbdud314@users.noreply.github.com>
2022-09-01 15:49:56 +0900

    Delete 토비의스프링 directory

--- 토비의스프링/app/src/main/java/com/tobyspring/app/dao/ConnectionMaker.java
+++
@@ -1,8 +0,0 @@
-package com.tobyspring.app.dao;
-
-import java.sql.Connection;
-import java.sql.SQLException;
-
-public interface ConnectionMaker {
-    public Connection makeConnection() throws SQLException;
-}
