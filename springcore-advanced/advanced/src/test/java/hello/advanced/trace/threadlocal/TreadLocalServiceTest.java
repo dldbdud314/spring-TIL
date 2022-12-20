1c49fe045d01e03287fba0f7709830d8b3058611
dldbdud314 <dldbdud314@naver.com>
2022-12-20 15:47:24 +0900

    동시성 문제 해결 - ThreadLocal 활용 코드

--- springcore-advanced/advanced/src/test/java/hello/advanced/trace/threadlocal/TreadLocalServiceTest.java
+++ springcore-advanced/advanced/src/test/java/hello/advanced/trace/threadlocal/TreadLocalServiceTest.java
@@ -1,20 +1,20 @@
 package hello.advanced.trace.threadlocal;
 
-import hello.advanced.trace.threadlocal.code.FieldService;
+import hello.advanced.trace.threadlocal.code.ThreadLocalService;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
 @Slf4j
-public class FieldServiceTest {
-    private final FieldService fieldService = new FieldService();
+public class TreadLocalServiceTest {
+    private final ThreadLocalService tlService = new ThreadLocalService();
 
     @Test
     void field() {
         log.info("main start");
         Runnable userA = () ->
-            fieldService.logic("userA");
+                tlService.logic("userA");
         Runnable userB = () ->
-            fieldService.logic("userB");;
+                tlService.logic("userB");;
 
         Thread threadA = new Thread(userA);
         threadA.setName("thread-A");
@@ -22,8 +22,7 @@ public class FieldServiceTest {
         threadB.setName("thread-B");
 
         threadA.start();
-        // sleep(2000); // 동시성 문제 발생 X
-        sleep(100); // 동시성 문제 발생
+        sleep(100); // 동시성 문제 발생 X
 
         threadB.start();
         sleep(3000);
