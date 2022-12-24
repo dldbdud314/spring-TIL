047931986a5e8992d146dd9906dc82fe1c634149
dldbdud314 <dldbdud314@naver.com>
2022-12-24 13:44:36 +0900

    전략 패턴 두번째: 파라미터로 넘기는 방식 추가

---
+++ springcore-advanced/advanced/src/test/java/hello/advanced/trace/strategy/ContextV2Test.java
@@ -0,0 +1,26 @@
+package hello.advanced.trace.strategy;
+
+import hello.advanced.trace.strategy.strategy.ContextV2;
+import hello.advanced.trace.strategy.strategy.StrategyLogic1;
+import hello.advanced.trace.strategy.strategy.StrategyLogic2;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class ContextV2Test {
+
+    @Test
+    void strategyV1(){
+        ContextV2 context = new ContextV2();
+        context.execute(new StrategyLogic1());
+        context.execute(new StrategyLogic2());
+    }
+
+    @Test
+    void strategyV2(){
+        ContextV2 context = new ContextV2();
+        context.execute(() -> log.info("비즈니스 로직 1 실행"));
+        context.execute(() -> log.info("비즈니스 로직 2 실행"));
+    }
+
+}
