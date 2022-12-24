29ae2de08be35b4bb43a33b6ecb813e37df46adc
dldbdud314 <dldbdud314@naver.com>
2022-12-24 13:11:23 +0900

    익명내부클래스와 람다 활용

--- springcore-advanced/advanced/src/test/java/hello/advanced/trace/strategy/ContextV1Test.java
+++ springcore-advanced/advanced/src/test/java/hello/advanced/trace/strategy/ContextV1Test.java
@@ -1,6 +1,7 @@
 package hello.advanced.trace.strategy;
 
 import hello.advanced.trace.strategy.strategy.ContextV1;
+import hello.advanced.trace.strategy.strategy.Strategy;
 import hello.advanced.trace.strategy.strategy.StrategyLogic1;
 import hello.advanced.trace.strategy.strategy.StrategyLogic2;
 import lombok.extern.slf4j.Slf4j;
@@ -46,6 +47,26 @@ public class ContextV1Test {
 
         StrategyLogic2 logic2 = new StrategyLogic2();
         ContextV1 contextV2 = new ContextV1(logic2);
+        contextV2.execute();
+    }
+
+    @Test
+    void strategyV2(){
+        // 익명 내부 클래스 활용
+        Strategy strategy1 = new Strategy() {
+            @Override
+            public void call() {
+                log.info("비즈니스 로직2 실행");
+            }
+        };
+        ContextV1 contextV1 = new ContextV1(strategy1);
+        log.info("strategyLogic1={}", strategy1.getClass());
+        contextV1.execute();
+
+        // 람다식 활용 : 인터페이스에 메소드가 한개만 있음
+        Strategy strategy2 = () -> log.info("비즈니스 로직2 실행");
+        ContextV1 contextV2 = new ContextV1(strategy2);
+        log.info("strategyLogic2={}", strategy2.getClass());
         contextV2.execute();
     }
 
