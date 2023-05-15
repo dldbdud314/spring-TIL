5fd31fe547ba8acae66004486eb1b05a9bc84158
dldbdud314 <dldbdud314@naver.com>
2023-05-15 23:21:43 +0900

    트랜잭션 추상화 - 1. 트랜잭션 매니저

---
+++ db-1/jdbc/src/main/java/hello/jdbc/service/MemberServiceV3_1.java
@@ -0,0 +1,44 @@
+package hello.jdbc.service;
+
+import  hello.jdbc.domain.Member;
+import hello.jdbc.repository.MemberRepositoryV3;
+import java.sql.SQLException;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
+
+/**
+ * 트랜잭션 - 트랜잭션 매니저
+ */
+@RequiredArgsConstructor
+@Slf4j
+public class MemberServiceV3_1 {
+
+    private final PlatformTransactionManager transactionManager;
+    private final MemberRepositoryV3 memberRepository;
+
+    public void accountTransfer(String fromId, String toId, int money){
+        TransactionStatus status = transactionManager.getTransaction(new DefaultTransactionDefinition());
+
+        try {
+            bizLogic(fromId, toId, money);  // 비즈니스 로직 실행
+            transactionManager.commit(status);
+        } catch (Exception e){
+            transactionManager.rollback(status);
+            throw new IllegalStateException(e);
+        }
+    }
+
+    private void bizLogic(String fromId, String toId, int money) throws SQLException {
+        Member fromMember = memberRepository.findById(fromId);
+        Member toMember = memberRepository.findById(toId);
+
+        memberRepository.update(fromId, fromMember.getMoney() - money);
+        if (toMember.getMemberId().equals("ex")) throw new IllegalStateException("이체 중 예외 발생");
+        memberRepository.update(toId, toMember.getMoney() + money);
+    }
+
+
+}
